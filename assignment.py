db49ad50b57a9004728a3915005e64ae48c1609d
mineynduati016-arch <mineynduati016@gmail.com>
2026-03-13 13:40:36 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,19 @@
-assignment.py
+#Question 4
+print("Hello AI student")
+#Question 5
+name="John"
+age=20
+city="Nairobi"
+print(f"Name:{name}")
+print(f"Age:{age}")
+print(f"City:"{city}")
+#Question 6
+languages=["Python","Java","C++","Javascript","Go"]
+print(languages[0])
+#Question 7
+student={
+       "name":"John",
+       "age":21,
+       "course":"AI"
+       }
+print(student["name"])
